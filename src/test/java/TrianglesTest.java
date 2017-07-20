d3b0ac5f206a3c50bc3e4bbaa5b3436067b8cafe
memetibryan <memetibryan@gmail.com>
2017-07-20 10:48:47 +0300

    tests contains unfixed bugs

--- src/test/java/TrianglesTest.java
+++ src/test/java/TrianglesTest.java
@@ -4,11 +4,8 @@ import static org.junit.Assert.*;
 public class TrianglesTest {
 
 	@Test
-	public void triangleName_triangleNameEqiulateral_userInput() {
-		Triangles checkName = new Triangles();
-		sideA = 1;
-		sideB = 1;
-		sideC = 1;
-		assertEquals(checkName.triangleName("Equilateral"));
-	}
+    public void newTriangle_instantiatesCorrectly() {
+    Triangles testTriangle = new Triangles(2, 4, 6);
+    assertEquals(true, testTriangle instanceof Triangles);
+   }
 }
